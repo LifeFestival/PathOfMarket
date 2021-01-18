e55b408510389f7d62aeefba1bdd8c7fa625bf6e
Nick Treskin <PzFireFly@gmail.com>
2021-01-18 11:21:34 +0300

    Changed URL

--- lib/utils/apiConstants.dart
+++ lib/utils/apiConstants.dart
@@ -2,6 +2,6 @@ const MAIN_URL = "https://poe.ninja/api/data/";
 const CATEGORY_CURRENCY_PATH = "currencyoverview";
 const CATEGORY_ITEM_PATH = "itemoverview";
 
-const CURRENT_LEAGUE_NAME = "Heist";
+const CURRENT_LEAGUE_NAME = "Ritual";
 const LANGUAGE_PATH_RU = "ru";
 const LANGUAGE_PATH_ENG = "en";
