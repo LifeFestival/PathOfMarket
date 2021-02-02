9f5d527146c2ac5be8928afec0334021125d6940
Nick Treskin <PzFireFly@gmail.com>
2021-02-02 13:45:24 +0300

    Refactoring

--- lib/ui/itemList.dart
+++ lib/ui/itemList.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:path_of_market/models/categoryModels.dart';
@@ -19,74 +17,78 @@ class ItemListWidget extends StatefulWidget {
 }
 
 class _ItemListWidgetState extends State<ItemListWidget> {
+  Icon _searchIcon = Icon(Icons.search);
+  Widget _titleWidget = Text('ItemsList');
 
-  Icon searchIcon = Icon(Icons.search);
-  Widget titleWidget = Text('ItemsList');
-
-  List<Item> _filteredNameList;
-
-  @override
-  void initState() {
-    _filteredNameList = widget._itemList;
-    super.initState();
-  }
+  String _filterString = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: titleWidget,
-        actions: [
-            IconButton(icon: searchIcon, onPressed: () {
-              setState(() {
-              if (this.searchIcon.icon == Icons.search) {
-                this.searchIcon = Icon(Icons.cancel);
-                this.titleWidget = TextField(
-                  onChanged: (String value) {
-                    setState(() {
-                      this._textSearchCallback(value);
-                    });
-                  },
-                  textInputAction: TextInputAction.go,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Search',
-                    hintStyle: TextStyle(
-                      color: Colors.white30
-                    )
-                  ),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0
-                  ),
-                );
-              } else {
-                this._filteredNameList = widget._itemList;
-                this.searchIcon = Icon(Icons.search);
-                this.titleWidget = Text('ItemsList');
-              }
-            });
-            }),
+        appBar: AppBar(
+          title: _titleWidget,
+          actions: [
+            IconButton(
+                icon: _searchIcon,
+                onPressed: () {
+                  setState(() {
+                    if (_searchIcon.icon == Icons.search) {
+                      _searchIcon = Icon(Icons.cancel);
+                      _titleWidget = TextField(
+                        onChanged: (String value) {
+                          setState(() {
+                            _textSearchCallback(value);
+                          });
+                        },
+                        textInputAction: TextInputAction.go,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Search',
+                            hintStyle: TextStyle(color: Colors.white30)),
+                        style: TextStyle(color: Colors.white, fontSize: 16.0),
+                      );
+                    } else {
+                      _filterString = '';
+                      _searchIcon = Icon(Icons.search);
+                      _titleWidget = Text('ItemsList');
+                    }
+                  });
+                }),
           ],
-      ),
-      body: Center(
-          child: ListView.builder(
-        itemCount: this._filteredNameList.length,
-        itemBuilder: _generateItemTile,
-        padding: EdgeInsets.symmetric(vertical: 3.0),
-      )),
-    );
+        ),
+        body: Center(child: _buildList()));
   }
 
   Widget _generateItemTile(BuildContext context, int index) {
-    var _item = this._filteredNameList[index];
+    Item _item = _filterItemList()[index];
 
     if (_item is CurrencyItem) {
       return CurrencyTile(_item);
-    } else return ItemTile(_item);
+    } else
+      return ItemTile(_item);
   }
 
   _textSearchCallback(String text) {
-    this._filteredNameList = widget._itemList.where((item) => item.itemName.toLowerCase().contains(text.toLowerCase())).toList();
+    _filterString = text;
+  }
+
+  Widget _buildList() {
+    List<Item> filteredList = _filterItemList();
+
+    return ListView.builder(
+      itemCount: filteredList.length,
+      itemBuilder: _generateItemTile,
+      padding: EdgeInsets.symmetric(vertical: 3.0),
+    );
+  }
+
+  List<Item> _filterItemList() {
+    if (_filterString == '') return widget._itemList;
+
+    return widget._itemList
+        .where((item) => item.itemName
+            .toLowerCase()
+            .contains(_filterString.toLowerCase()))
+        .toList();
   }
 }
